5aca4f59456baa3a70193ae1fde6d4823b21e161
Tudor Gabriel <tudordangabriel28@gmail.com>
2024-04-10 14:51:10 +0300

    ATGR-63: Created models.

---
+++ AnytimeGear/AnytimeGear.Server/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿namespace AnytimeGear.Server.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Brand { get; set; }
+        public required string Model { get; set; }
+        public required string Description { get; set; }
+        public short Price { get; set; }
+        public short Quantity { get; set; }
+        public short ReplacementValue { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public required SubCategory SubCategory { get; set; }
+        public List<Rental> Rentals { get; set; }
+    }
+}
