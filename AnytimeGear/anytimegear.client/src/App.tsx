3c3f1ef7d90050243a03efecfd3e6608d77bd5ac
Tudor Gabriel <tudordangabriel28@gmail.com>
2024-04-18 00:59:59 +0300

    ATGR-73: Change Prettier formatting rules
    - Format all files

--- AnytimeGear/anytimegear.client/src/App.tsx
+++ AnytimeGear/anytimegear.client/src/App.tsx
@@ -1,14 +1,14 @@
-import { Outlet } from "react-router-dom";
-import "./App.css";
-import NavigationBar from "./components/NavigationBar/NavigationBar";
+import { Outlet } from 'react-router-dom'
+import './App.css'
+import NavigationBar from './components/NavigationBar/NavigationBar'
 
 function App() {
-  return (
-    <div>
-      <NavigationBar />
-      <Outlet />
-    </div>
-  );
+    return (
+        <div>
+            <NavigationBar />
+            <Outlet />
+        </div>
+    )
 }
 
-export default App;
+export default App
