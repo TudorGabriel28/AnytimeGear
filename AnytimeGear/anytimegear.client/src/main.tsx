f0bccdf1e6bbfa8e800725cdaa14de0d71d69a43
Tudor Gabriel <61846679+TudorGabriel28@users.noreply.github.com>
2024-04-18 16:03:10 +0300

    Removed datepicker code (#10)

--- AnytimeGear/anytimegear.client/src/main.tsx
+++ AnytimeGear/anytimegear.client/src/main.tsx
@@ -3,14 +3,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import './index.css'
-import { LocalizationProvider } from '@mui/x-date-pickers'
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import { router } from './utils/router'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
             <RouterProvider router={router} />
-        </LocalizationProvider>
     </React.StrictMode>
 )
