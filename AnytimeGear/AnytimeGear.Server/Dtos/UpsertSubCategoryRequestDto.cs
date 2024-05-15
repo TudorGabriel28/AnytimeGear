947b73c2e9b38722d452058dae91e786815c8215
Tudor Gabriel <61846679+TudorGabriel28@users.noreply.github.com>
2024-05-15 21:28:00 +0300

    Hotfix 1 (#14)
    * Rename SubCategory.cs to Subcategory.cs
    
    * Rename SubCategoryRepository.cs to SubcategoryRepository.cs
    
    * Rename ISubCategoryRepository.cs to ISubcategoryRepository.cs
    
    * Delete AnytimeGear/AnytimeGear.Server/Dtos/UpsertSubCategoryRequestDto.cs
    
    * Delete AnytimeGear/AnytimeGear.Server/Dtos/SubCategoryResponseDto.cs
    
    * Delete AnytimeGear/AnytimeGear.Server/Controllers/SubCategoriesController.cs
    
    * Rename AddSubCategory.tsx to AddSubcategory.tsx
    
    * Rename SubCategories.tsx to Subcategories.tsx

--- AnytimeGear/AnytimeGear.Server/Dtos/UpsertSubCategoryRequestDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace AnytimeGear.Server.Dtos;
-
-public class UpsertSubcategoryRequestDto
-{
-    public required string Name { get; set; }
-    public required string CategoryName { get; set; }
-}
