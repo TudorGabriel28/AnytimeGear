507cfa498930ca56b2d3cc9b53ff763b1c7e2dee
Tudor Gabriel <tudordangabriel28@gmail.com>
2024-05-16 19:45:09 +0300

    ATGR-62: Create Unit Tests

---
+++ AnytimeGear/UnitTests/Mocks/IProductRepositoryMock.cs
@@ -0,0 +1,60 @@
+﻿using AnytimeGear.Server.Data;
+using AnytimeGear.Server.Models;
+using AnytimeGear.Server.Repositories;
+using AnytimeGear.Server.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace UnitTests.Mocks;
+public class IProductRepositoryMock
+{
+    public static IProductRepository GetMock()
+    {
+        List<Product> lstProduct = GenerateTestData();
+        AnytimeGearServerContext dbContextMock = DbContextMock.GetMock<Product, AnytimeGearServerContext>(lstProduct, x => x.Product);
+        return new ProductRepository(dbContextMock);
+    }
+
+    private static List<Product> GenerateTestData()
+    {
+        List<Product> lstProduct = new();
+        Random rand = new Random();
+
+        Category category = new Category
+        {
+            Id = 1,
+            Name = "Category 1"
+        };
+
+        Subcategory subcategory = new Subcategory
+        {
+            Id = 1,
+            Name = "Subcategory 1",
+            CategoryId = category.Id,
+            Category = new Category { Id = 1, Name = "Category 1" }
+        };
+
+        for (int index = 1; index <= 10; index++)
+        {
+            lstProduct.Add(new Product
+            {
+                Id = 1,
+                Name = "Product 1",
+                Brand = "Brand A",
+                Model = "Model A",
+                Description = "Description 1",
+                Price = 100,
+                Capacity = 10,
+                ReplacementValue = 500,
+                CreatedAt = DateTime.Now,
+                Subcategory = subcategory,
+                Rentals = []
+            });
+        }
+        return lstProduct;
+    }
+}
---
+++ AnytimeGear/UnitTests/ProductControllerTest.cs
@@ -0,0 +1,73 @@
+﻿using AnytimeGear.Server.Controllers;
+using AnytimeGear.Server.Dtos;
+using AnytimeGear.Server.Models;
+using AnytimeGear.Server.Repositories.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests;
+
+[TestClass]
+public class ProductsControllerTests
+{
+    [TestMethod]
+    public async Task RetrieveProducts_ReturnsExpectedResult()
+    {
+        // Arrange
+        var mockProductRepository = new Mock<IProductRepository>();
+        var mockMapper = new Mock<IMapper>();
+        var mockRequest = new RetrieveProductsRequestDto
+        {
+            StartDate = DateTime.Now.ToString(),
+            EndDate = DateTime.Now.AddDays(2).ToString(),
+            Quantity = 1,
+            SubcategoryId = 1,
+            SortKey = "price",
+            SortOrder = "asc",
+        };
+        var mockProducts = new List<ProductResponseDto>
+        {
+            new ProductResponseDto { 
+                Name = "Test",
+                Brand = "Brand A",
+                Model = "Model A",
+                Description = "Description 1",
+                Price = 100,
+                Capacity = 10,
+                Subcategory = new Subcategory { Id= 1, Name = "Subcategory 1" , Category = new Category { Name = "Category 1", Id= 2 }, CategoryId=2 },
+            },
+        };
+        var mockProductBrands = new List<ProductBrandDto>
+        {
+            new ProductBrandDto { Name="Brand 1", Count=1 },
+        };
+
+        mockProductRepository.Setup(repo => repo.GetAllAsync(mockRequest)).ReturnsAsync(mockProducts);
+        mockProductRepository.Setup(repo => repo.GetBrandsAsync(mockRequest)).ReturnsAsync(mockProductBrands);
+
+        var controller = new ProductsController(mockProductRepository.Object, mockMapper.Object);
+
+        // Act
+        var result = await controller.RetrieveProducts(mockRequest);
+
+        var okResult = result.Result as OkObjectResult;
+        var response = okResult.Value as ProductListResponseDto;
+
+        // Assert
+        Assert.IsInstanceOfType(okResult, typeof(OkObjectResult));
+        Assert.IsInstanceOfType(response, typeof(ProductListResponseDto));
+
+        Assert.AreEqual(mockProducts.Count, response.TotalCount);
+        Assert.AreEqual(mockProducts.Min(p => p.Price), response.MinPrice);
+        Assert.AreEqual(mockProducts.Max(p => p.Price), response.MaxPrice);
+        Assert.AreEqual(mockRequest.SortKey, response.SortKey);
+        Assert.AreEqual(mockRequest.SortOrder, response.SortOrder);
+        Assert.AreEqual(mockRequest.CheckedBrandNames, response.CheckedBrandNames);
+    }
+}
