34340cc9b62a9436db8370ee6c169bea4b749bac
Tudor Gabriel <tudordangabriel28@gmail.com>
2024-05-16 20:04:31 +0300

    ATGR-62: Create Unit Tests - Removed leftovers from repository mock

--- AnytimeGear/AnytimeGear.Server/Data/AnytimeGearServerContext.cs
+++ AnytimeGear/AnytimeGear.Server/Data/AnytimeGearServerContext.cs
@@ -5,18 +5,14 @@ namespace AnytimeGear.Server.Data;
 
 public class AnytimeGearServerContext : DbContext
 {
-    public AnytimeGearServerContext()
-    {
-
-    }
-
+   
     public AnytimeGearServerContext(DbContextOptions<AnytimeGearServerContext> options)
         : base(options)
     {
     }
 
     public DbSet<User> User { get; set; } = default!;
-    public virtual DbSet<Product> Product { get; set; } = default!;
+    public DbSet<Product> Product { get; set; } = default!;
     public DbSet<Category> Category { get; set; } = default!;
     public DbSet<Subcategory> Subcategory { get; set; } = default!;
     public DbSet<Rental> Rental { get; set; } = default!;
