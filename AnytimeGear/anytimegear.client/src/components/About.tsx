0ec82de8fd2a10e11a6e58911d6335a9a003a6e1
UntaruNarcis <untaru.narcis.z3f@student.ucv.ro>
2024-05-29 20:57:33 +0300

    -Added icon photo and home photo -Fixed Contact Us page

--- AnytimeGear/anytimegear.client/src/components/About.tsx
+++ AnytimeGear/anytimegear.client/src/components/About.tsx
@@ -1,8 +1,10 @@
 function About() {
     return (
         <div className="container about-us-page">
-            <div className="row">
+            <div className="title">
                 <h1 className="title">About AnytimeGear</h1>
+            </div>
+            <div className="row">
                 <div className="col-lg-6">
                     <div className="card p-3 shadow border-top border-2 rounded h-100">
                         <div className="card-body">
@@ -28,42 +30,33 @@ function About() {
                 <div className="col-lg-6">
                     <div className="card p-3 shadow border-top border-2 rounded h-100">
                         <div className="card-body">
-                            <h2 className="card-title">Our Mission</h2>
-                            <div className="card-text">
-                                <p>To democratize sports and outdoor activities by removing barriers to equipment access, enabling people to explore their passions without constraints.</p>
-                                <p>Our Vision is to become the go-to destination for sports equipment rentals globally, fostering a community of adventurers who share our love for exploration and active living.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-lg-6">
-                    <div className="card p-3 shadow border-top border-2 rounded h-100">
-                        <div className="card-body">
                             <h2 className="card-title">Our Values</h2>
                             <p className="card-text">
-                                Accessibility: We believe that everyone should have the opportunity to experience the joy of sports, regardless of their background or expertise.
-                                Quality: We're committed to providing high-quality equipment that meets the diverse needs of our customers, ensuring safety and performance.
-                                Innovation: We continuously strive to innovate and improve our services, staying ahead of the curve in the ever-evolving sports industry.
+                                Accessibility: We believe that everyone should have the opportunity to experience the joy of sports, regardless of their background or expertise.<br />
+                                Quality: We're committed to providing high-quality equipment that meets the diverse needs of our customers, ensuring safety and performance.<br />
+                                Innovation: We continuously strive to innovate and improve our services, staying ahead of the curve in the ever-evolving sports industry.<br />
                                 Customer Satisfaction: Your satisfaction is our priority. We're dedicated to delivering exceptional service and support at every step of your renting journey.
                             </p>
                         </div>
                     </div>
                 </div>
             </div>
-            <div className="row">
+            <div className="title">
                 <h1 className="title">Contact Us</h1>
-                <div className="col-lg-3">
-                </div>
-                <div id="contact-section" className="col-lg-6">
+            </div>
+            <div className="row justify-content-center">
+                
                     <div className="card p-3 shadow border-top border-2 rounded h-100">
-                        <div className="card-body">
+                    <div className="row">
                             <p className="card-text">
                                 <strong>AnytimeGear</strong><br />
                                 <strong>Address:</strong> 123 Main Street, Anytown, USA<br />
                                 <strong>Phone:</strong> 555-123-4567<br />
                                 <strong>Email:</strong> info@anytimegear.com
-                            </p>
+                        </p>
+                            <div>
                             <h2 className="card-title">Connect With Us</h2>
+                        </div>
                             <p className="card-text">
                                 Stay updated on the latest news and offers by following us on social media:<br />
                                 <a href="#" className="card-link">Facebook</a><br />
@@ -73,11 +66,8 @@ function About() {
                         </div>
                     </div>
                 </div>
-                <div className="col-lg-3">
-                </div>
             </div>
-        </div>
     );
 }
 
-export default About
+export default About;
